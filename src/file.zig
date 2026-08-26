70c84928988c932b74a48f328b6f8b5789b7a013
NJ <226465585+nj-twice@users.noreply.github.com>
2026-08-26 21:43:46 +0200

    List files in the CWD
    In the process, I finally figured out basic memory allocation in Zig.

---
+++ src/file.zig
@@ -0,0 +1,39 @@
+const root = @import("root");
+const std = @import("std");
+
+pub const FileList = std.ArrayList(File);
+pub const File = []const u8;
+
+/// List files in the current directory.
+/// For now, it is meant to only run once, at startup.
+pub fn ls(init: std.process.Init) FileList {
+    const cwd_handle = std.Io.Dir.cwd();
+    const cwd_open = std.Io.Dir.openDir(cwd_handle, init.io, ".", .{ .iterate = true }) catch |err| {
+        std.log.err("Error: {}\n", .{err});
+        @panic("Couldn't open CWD!\n");
+    };
+    defer cwd_open.close(init.io);
+
+    // We use gpa because it's a small local allocation.
+    // What we actually need will be copied later.
+    var dirwalker = std.Io.Dir.walkSelectively(cwd_open, init.gpa) catch unreachable;
+    defer dirwalker.deinit();
+
+    var filelist: FileList = .empty;
+
+    var entry = dirwalker.next(init.io) catch unreachable;
+    while (entry != null) {
+        // A subsequent call to next() renders the filename slice invalid memory.
+        // We need to copy the bytes to owned slices.
+        const filename = entry.?.basename;
+        const filename_copy = std.mem.Allocator.dupe(init.arena.allocator(), u8, filename) catch unreachable;
+
+        filelist.append(init.arena.allocator(), filename_copy) catch |err| {
+            std.log.err("Error: {}\n", .{err});
+            @panic("Error\n");
+        };
+        entry = dirwalker.next(init.io) catch unreachable;
+    }
+
+    return filelist;
+}
--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,38 @@
 const rl = @import("raylib");
+const file = @import("file.zig");
 const std = @import("std");
 
 const SCREEN_WIDTH = 1200;
 const SCREEN_HEIGHT = 800;
 const NAME = "Poorsprite";
 
-pub fn main() void {
+const MAX_LOADED_FILES = 1;
+
+const Data = struct {
+    show_menu: bool = true,
+    select_idx: i32 = 0,
+    files: file.FileList,
+
+    fn create(init: std.process.Init) Data {
+        const files = file.ls(init);
+        return Data{
+            .files = files,
+        };
+    }
+
+    // De-init allocated memory
+    fn drop(self: *Data, init: std.process.Init) void {
+        // The Init doc says that only arena is freed automatically.
+        // It says nothing about gpa.
+        // Since we properly use arena for long-living allocations,
+        // we likely won't have to use this function.
+        // We keep it nonetheless, just in case.
+        _ = self;
+        _ = init;
+    }
+};
+
+pub fn main(init: std.process.Init) !void {
     // Work around bug: https://codeberg.org/ziglang/zig/issues/35512
     _ = std.debug.lockStderr(&.{});
     std.debug.unlockStderr();
@@ -16,9 +43,37 @@ pub fn main() void {
     defer rl.CloseAudioDevice();
     rl.SetTargetFPS(60);
 
+    var data = Data.create(init);
+    defer data.drop(init);
+
     while (!rl.WindowShouldClose()) {
+        update(&data);
+
         rl.BeginDrawing();
         defer rl.EndDrawing();
         defer rl.ClearBackground(rl.BLACK);
+
+        draw(&data);
     }
 }
+
+fn update(data: *Data) void {
+    _ = data; // autofix
+}
+
+fn draw(data: *const Data) void {
+    if (data.show_menu) {
+        drawSelectionList(data);
+        drawLoadedList();
+    }
+}
+
+fn drawSelectionList(data: *const Data) void {
+    const files = data.files;
+    for (0..files.items.len) |i| {
+        std.debug.print("File: {s}\n", .{files.items[i]});
+    }
+    std.debug.print("-----\n", .{});
+}
+
+fn drawLoadedList() void {}
