201c3671f7cbbf5ec20137333408ea19b664d227
NJ <226465585+nj-twice@users.noreply.github.com>
2026-09-11 11:09:30 +0200

    Add FPS field instead of hardcoded timer threshold value

--- src/viewer.zig
+++ src/viewer.zig
@@ -4,19 +4,23 @@
 const file = @import("file.zig");
 const frame = @import("frame.zig");
 const rl = @import("raylib");
+const debug = @import("std").debug;
 
 pub const Data = struct {
     frames: frame.FrameList = .empty,
     current_frame: ?u32 = null,
     timer: f32 = 0.0,
+    fps: u16 = 2, // MUST NOT BE ZERO
 };
 
 pub fn update(data: *Data) void {
+    debug.assert(data.fps != 0);
+
     if (data.frames.items.len == 0) return;
     if (data.current_frame == null) return;
 
     data.timer += rl.GetFrameTime();
-    if (data.timer > 0.5) {
+    if (data.timer > 1.0 / @as(f32, @floatFromInt(data.fps))) {
         data.timer = 0;
         data.current_frame.? = @mod(
             data.current_frame.? + 1,
